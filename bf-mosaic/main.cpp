caeb2882afd6200203c849bcde104ed7f048a33a
Gubbish <gubbish@Gubbishs-MacBook-Air.local>
2012-10-01 16:30:37 -0700

    added xcode project

---
+++ bf-mosaic/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  bf-mosaic
+//
+//  Created by Gubbish on 10/1/12.
+//  Copyright (c) 2012 Gubbish. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
